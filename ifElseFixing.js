c90ef345baa1da5ae3ff29998d0ca51d1e030643
Sabrina Paschini <sabrina.paschini@hotmail.com>
2023-09-04 23:14:16 -0300

    fixing

---
+++ ifElseFixing.js
@@ -0,0 +1,18 @@
+// sintaxe basica 
+// if (condicao){comando}
+//else if igual  
+//else somente o comando {}
+
+
+let nome="sabrina"
+
+if(nome == "sabrina"){
+    console.log("o nome dela é sabrina.")
+
+}else if(nome == "joana"){
+    console.log("o nome dela é joana")
+
+}else{
+    console.log("ela tem outro nome")
+}
+
---
+++ while.js
@@ -0,0 +1,19 @@
+/*let x= 0
+
+while(x <= 5){
+    console.log("Testando repetição")
+//incrementador 
+x++
+}
+
+*/
+
+let palavra = "sabrina";
+var i= 0;
+
+while (i<= 6){
+    console.log(palavra[i]);
+    i +=1;
+}
+
+
